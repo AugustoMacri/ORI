f462cc577875abe60fd6bb031938c0cf65a10b2f
Augusto Macri <augusto.macri@hotmail.com>
2023-11-09 11:42:59 -0300

    Trabalho 2

--- Trabalho 2/modelo_vetorial.py
+++ Trabalho 2/modelo_vetorial.py
@@ -100,40 +100,46 @@ print(consulta)
 
 def tokenizar_consulta(consulta):
     tokens = nltk.word_tokenize(consulta)
-    return tokens
+    return tokens #retorna consulta tokenizada
 
 tokens = tokenizar_consulta(ler_consulta(arquivoConsulta))
 print(tokens)
 
-# Cálculo do TF
-TF = {}
-for termo, documentos in indice_invertido.items():
-    for doc, freq in documentos:
-        tf_termo_no_doc = 1 + math.log10(freq)
-        if doc not in TF:
-            TF[doc] = {termo: tf_termo_no_doc}
-        else:
-            TF[doc][termo] = tf_termo_no_doc
-#print(TF)
-
-# Cálculo do IDF
-num_documentos_total = len(lista_caminhos)
 IDF = {}
-for termo, documentos in indice_invertido.items():
-    idf_termo = math.log10(num_documentos_total / len(documentos))
-    IDF[termo] = idf_termo
-#print(IDF)
-
-# Cálculo do TF-IDF
 TF_IDF = {}
-for doc, termos in TF.items():
-    TF_IDF[doc] = {}
-    for termo, tf in termos.items():
-        TF_IDF[doc][termo] = tf * IDF[termo]
-print(TF_IDF)
+# Cálculo do TF-IDF
+def calculcar_tf_idf(tokens, indice_invertido):
+        
+    # Cálculo do TF
+    TF = {}
+    for termo, documentos in indice_invertido.items():
+        for doc, freq in documentos:
+            tf_termo_no_doc = 1 + math.log10(freq)
+            if doc not in TF:
+                TF[doc] = {termo: tf_termo_no_doc}
+            else:
+                TF[doc][termo] = tf_termo_no_doc
+    #print(TF)
+
+    # Cálculo do IDF
+    num_documentos_total = len(lista_caminhos)
+    
+    for termo, documentos in indice_invertido.items():
+        idf_termo = math.log10(num_documentos_total / len(documentos))
+        IDF[termo] = idf_termo
+    #print(IDF)
+
+    # Cálculo do TF-IDF
+    for doc, termos in TF.items():
+        TF_IDF[doc] = {}
+        for termo, tf in termos.items():
+            TF_IDF[doc][termo] = tf * IDF[termo]
+    print(TF_IDF)
+    
 
 # Gravar dados dos pesos no arquivo pesos.txt
 dados_pesos = {}
+calculcar_tf_idf(tokens, indice_invertido)
 for doc, termos in TF_IDF.items():
     dados_docs = []
     for termo, peso in termos.items():
@@ -153,42 +159,16 @@ with open('pesos.txt', 'w') as arquivo_pesos:
 nome_para_indice = {nome_arquivo: indice + 1 for indice, nome_arquivo in enumerate(lista_caminhos)}
 print(nome_para_indice)
 
-# Calculo de Similaridade
-similaridade_resultados = []
-
-for doc, termos in TF_IDF.items():  # Itera sobre todos os doc na base
-    consulta_vetor = {}
-    for termo in tokens:
-        termo_stem = stemmer.stem(termo.lower())
-        if termo_stem in termos:  # Verifica se tem o termo no doc
-            consulta_vetor[termo_stem] = termos[termo_stem]
-
-    similaridade = 0
-    for termo, peso in termos.items():
-        if termo in consulta_vetor:
-            similaridade += peso * consulta_vetor[termo]
-
-    #diminui para 0.00001 porque nenhum arquivo aparecia do resposta.txt !!!! lembrar de mudar 
-    if similaridade >= 0.001: #Faz a filtragem dos resultados da similaridade que sao >= a 0.001
-        similaridade_resultados.append((lista_caminhos[doc - 1], similaridade))
-
-# Ordena os resultados por similaridade
-similaridade_resultados.sort(key=lambda x: x[1], reverse=True)
-
 
 # trecho de avaliar a consulta com os simbolos do codigo de mod booleano 
 def avaliar_consulta(tokens, indice_invertido):
-    resultado = set()  # inicializa como um conjunto vazio
+    resultado = set()  # Inicializa como um conjunto vazio
     operador = None
     for token in tokens:
         if token == '&':
             operador = 'and'
-        elif token == '|':
-            operador = 'or'
-        elif token == '!':
-            operador = 'not'
         else:
-            termo_stem = stemmer.stem(token.lower())  # aplica stemming
+            termo_stem = stemmer.stem(token.lower())  # Aplica stemming
             if operador is None:
                 if termo_stem in indice_invertido:
                     resultado = set([doc for doc, freq in indice_invertido[termo_stem]])
@@ -201,36 +181,57 @@ def avaliar_consulta(tokens, indice_invertido):
                     documentos_termo = set()
                 if operador == 'and':
                     resultado = resultado.intersection(documentos_termo)
-                elif operador == 'or':
-                    resultado = resultado.union(documentos_termo)
-                elif operador == 'not':
-                    resultado.difference_update(documentos_termo)
             operador = None
     return resultado
 
 resultado = avaliar_consulta(tokens, indice_invertido)
 print(resultado)
 
-# Ordena os resultados por similaridade
-similaridade_resultados.sort(key=lambda x: x[1], reverse=True)
 
-# Grava os resultados no arquivo resposta.txt
-with open('resposta.txt', 'w') as arquivo_resposta:
-    arquivo_resposta.write(f"{len(similaridade_resultados)}\n")
-    for doc, similaridade in similaridade_resultados:
-        arquivo_resposta.write(f"{doc} {similaridade:.4f}\n")
-        print("resposta.txt")
-        print(f"{doc} {similaridade:.4f}")
+# Cálculo da similaridade
+def calcular_similaridade(consulta, lista_caminhos, nome_para_indice, IDF, TF_IDF):
+    similaridade_result = []
 
-"""
-03/11
-Estava funcionando perfeitamente para a consulta 1
-mas não funciona para a consulta 2 porque ela tem o & igual no cod do booleano
-lembrar de pegar aquele trecho 
-04/11
-tambem ficava preso so no primeiro doc pq tava TF_IDF[1][termo_stem] no calc da similaridade
-ai fazia com que dasse 0 na segunda consulta tbm
-6/11
-se eu mudar a consulta.txt para engracada ou amor, sempre vai devolver 0.2276
-mas se a consulta for casa, ele devolve 0 sempre 
-"""
+    # Calc o vetor de consulta
+    vetor_de_consulta = {}
+    consulta_tokens = tokenizar_consulta(consulta)
+
+    for termo in consulta_tokens:
+        termo_stem = stemmer.stem(termo.lower())
+        if termo_stem in IDF:
+            if termo_stem in vetor_de_consulta:
+                vetor_de_consulta[termo_stem] += 1  # Incrementa a frequência do termo na consulta
+            else:
+                vetor_de_consulta[termo_stem] = 1  # Inicia a frequência do termo na consulta como 1
+
+    # Calcular a similaridade para cada documento
+    for doc, termos in TF_IDF.items():
+        similaridade = 0
+        modulo_vetor_doc = 0
+
+        for termo, TF_IDF in termos.items():
+            if termo in vetor_de_consulta:
+                similaridade += TF_IDF * vetor_de_consulta[termo]
+            modulo_vetor_doc += TF_IDF ** 2
+
+        modulo_vetor_doc = math.sqrt(modulo_vetor_doc)
+        modulo_vetor_consulta = math.sqrt(sum(v ** 2 for v in vetor_de_consulta.values()))
+
+        if modulo_vetor_doc != 0 and modulo_vetor_consulta != 0:
+            similaridade = similaridade / (modulo_vetor_doc * modulo_vetor_consulta) #A conta em si é feita aqui 
+
+        if similaridade >= 0.001: #só vai adicionar a lista em que a similaridade e >= 0.001
+            nome_documento = lista_caminhos[doc - 1]
+            similaridade_result.append((nome_documento, similaridade))
+
+    similaridade_result.sort(key=lambda x: x[1], reverse=True)
+
+    with open('resposta.txt', 'w') as arquivo_resposta:
+        arquivo_resposta.write(f'{len(similaridade_result)}\n')
+        for nome_doc, similaridade in similaridade_result:
+            arquivo_resposta.write(f'{nome_doc} {similaridade:.4f}\n')
+
+    return similaridade_result
+
+similaridades = calcular_similaridade(consulta, lista_caminhos, nome_para_indice, IDF, TF_IDF)
+print(similaridades)
