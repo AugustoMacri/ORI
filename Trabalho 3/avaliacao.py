855cda027e8b57dfdcca923ab1b5feae390201df
Augusto Macri <augusto.macri@hotmail.com>
2023-11-29 16:54:46 -0300

    Trabalho 3

--- Trabalho 3/avaliacao.py
+++ Trabalho 3/avaliacao.py
@@ -4,9 +4,7 @@ Modelo de avalicação com gráficos
 Nome: Augusto Fernandes Macri
 Matrícula: 12111BSI221
 
-matplotlib n funcionava, baixar:
-pip install matplotlib
-
+Os cálculos e gráficos foram realizados na mão antes de serem implementados no código para maior acurácia, a resolução pode ser vista em: https://github.com/AugustoMacri/ORI/tree/main/Trabalho%203
 """
 import sys
 import matplotlib.pyplot as plt
@@ -39,84 +37,85 @@ if __name__ == "__main__": #verifica se o programa está sendo executado diretam
 #print(respostas_ideais)
 #print(respostas_sistema)
 
-#Calcular Precisão e Revocação para cada consulta 
+#Calcular prec e revoc para cada consulta
 def calcular_prec_revoc(respostas_ideal, resposta_sistema):
-    documentos_recuperados = len(resposta_sistema)
-    documentos_relevantes_recuperados = len(set(resposta_sistema).intersection(set(respostas_ideal)))
+    precisoes = []
+    revocacoes = []
 
-    precisao = documentos_relevantes_recuperados / documentos_recuperados if documentos_recuperados > 0 else 0 #calculo da precisao para cada consulta
-    revocacao = documentos_relevantes_recuperados / len(respostas_ideal) if len(respostas_ideal) > 0 else 0 #calculo da revocacao para cada consulta
+    for termo in resposta_sistema:
+        if termo in respostas_ideal:
+            documentos_recuperados = len(resposta_sistema[:resposta_sistema.index(termo)+1])
+            documentos_relevantes_recuperados = len(set(resposta_sistema[:resposta_sistema.index(termo)+1]).intersection(set(respostas_ideal)))
 
-    return precisao, revocacao
+            precisao = documentos_relevantes_recuperados / documentos_recuperados if documentos_recuperados > 0 else 0
+            revocacao = documentos_relevantes_recuperados / len(respostas_ideal) if len(respostas_ideal) > 0 else 0
 
+            precisoes.append(precisao)
+            revocacoes.append(revocacao)
 
-result_precisao = []
-result_revoc = []
-
-for i in range(num_consultas):
-    precisao, revocacao = calcular_prec_revoc(respostas_ideais[i], respostas_sistema[i])
-    result_precisao.append(precisao)
-    result_revoc.append(revocacao)
-    print(f"Consulta {i + 1}: Precisão: {precisao}, Revocação: {revocacao}")
+    return precisoes, revocacoes
 
 
-#Interpolação da prec e revoc
-niveis_revocacao = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]# Níveis de revocação de 0 a 1 em incrementos de 0.1
-def interpolar_valores(precisao, revocacao):
+
+# Interpolação da prec e revoc
+niveis_revocacao = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+def interpolar_valores(precisoes, revocacoes):
+    #lista com prec e revoc
+    dados = list(zip(revocacoes, precisoes))
+    dados.sort(reverse=True)  #revoc decrescente
+
     valores_interp = []
 
-    precisao_ordenada, revocacao_ordenada = zip(*sorted(zip(precisao, revocacao), key=lambda x: x[1]))
-
-    print(f"Precis ord: {precisao_ordenada}")
-    print(f"Revoc ord {revocacao_ordenada}")
-
     for nivel in niveis_revocacao:
-        valores_nivel = [p for r, p in zip(revocacao_ordenada, precisao_ordenada) if r >= nivel] #zip faz com que seja criado uma lista de tuplas onde cada tupla contem um par (revoc, prec)
-        if valores_nivel:
-            closest_value = min(valores_nivel, key=lambda x: abs(x - nivel))
-            valores_interp.append(closest_value)
+        prec_interpoladas = [prec for rev, prec in dados if rev >= nivel]  #Filtrar a maior prec
+        if prec_interpoladas:
+            valor_interp = max(prec_interpoladas)  #maior prec para o nivel
+            valores_interp.append(valor_interp)
         else:
-            valores_interp.append(0)
-
-        print(f"Nível de revocação: {nivel}, Valores de precisão: {valores_nivel}")
+            valores_interp.append(0)  #se nao tiver precisao colocar em 0
 
     return valores_interp
 
-"""print("!!!!Valores apos a interpolacao!!!!")
-valores_interpolados = interpolar_valores(result_precisao, result_revoc)
-print("Valores interpolados:", valores_interpolados)"""
 
-
-#Interpolação da prec e revoc para cada consulta (na referencia e para sair 3 consultas)
 valores_interpolados_por_consulta = []
-
 for i in range(num_consultas):
-    valores_interp = interpolar_valores([result_precisao[i]], [result_revoc[i]])
+    precisoes, revocacoes = calcular_prec_revoc(respostas_ideais[i], respostas_sistema[i])
+    
+    # interp para cada consulta
+    valores_interp = interpolar_valores(precisoes, revocacoes)
     valores_interpolados_por_consulta.append(valores_interp)
 
-#Plotar os graficos (graficos da consulta, nao o da media)
-for i, valores in enumerate(valores_interpolados_por_consulta, start=1):
-    plt.figure()  #Tem que criar uma fig nova para cada grafico, antes tava criando uma com todas as consultas 
-    plt.plot(niveis_revocacao, valores, marker='o', linestyle='-', label=f'Consulta {i}')
-    plt.xlabel('Revocação')
-    plt.ylabel('Precisão')
-    plt.title(f'Gráfico de Precisão-Revocação para Consulta {i}')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    #print(valores_interp)
 
-#Calcular a media 
+#calcular a media dos valores 
 media_precisao = np.mean(valores_interpolados_por_consulta, axis=0)
-#Plotar o grafico da media 
-plt.plot(niveis_revocacao, media_precisao, marker='o', linestyle='-', label='Média de Todas as Consultas')
-plt.xlabel('Revocação')
-plt.ylabel('Precisão')
-plt.title('Média de Precisão-Revocação para Todas as Consultas')
-plt.legend()
-plt.grid(True)
+
+#Plotação dos graficos
+#disposiçao dos graficos 
+num_linhas = 2
+num_colunas = 2
+
+#todos numa unica janela
+fig, axs = plt.subplots(num_linhas, num_colunas, figsize=(12, 10))
+
+#Ajuste de posicao
+for i, valores in enumerate(valores_interpolados_por_consulta[:num_linhas*num_colunas], start=1):
+    linha = (i - 1) // num_colunas
+    coluna = (i - 1) % num_colunas
+    axs[linha, coluna].plot(niveis_revocacao, valores, marker='o', linestyle='-', label=f'Consulta {i}')
+    axs[linha, coluna].set_xlabel('Revocação')
+    axs[linha, coluna].set_ylabel('Precisão')
+    axs[linha, coluna].set_title(f'Gráfico de Precisão-Revocação para Consulta {i}')
+    axs[linha, coluna].legend()
+    axs[linha, coluna].grid(True)
+
+# Plotar o gráfico da média na última posição
+axs[num_linhas-1, num_colunas-1].plot(niveis_revocacao, media_precisao, marker='o', linestyle='-', label='Média de Todas as Consultas')
+axs[num_linhas-1, num_colunas-1].set_xlabel('Revocação')
+axs[num_linhas-1, num_colunas-1].set_ylabel('Precisão')
+axs[num_linhas-1, num_colunas-1].set_title('Média de Precisão-Revocação para Todas as Consultas')
+axs[num_linhas-1, num_colunas-1].legend()
+axs[num_linhas-1, num_colunas-1].grid(True)
+
+plt.tight_layout()
 plt.show()
-
-"""
-Os calculos estão errados, os gráficos não dão igual aos do enunciado 
-
-"""
